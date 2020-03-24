89341de97d243705c8c3cdbf07089d6b3ed1c61e
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-03-24 14:52:42 -0500

    working on figuring out the sort functionality

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
-import React, { useEffect } from 'react';
-import employeeTable from "./components/User-table";
+import React, { useEffect, useState } from 'react';
+// import TableFilter from 'react-table-filter';
 import './App.css';
 
 function App() {
-  const (users, setUsers) = usestate([]);
+  const [users, setUsers] = useState([]);
+  // const [updatedData, setUpdatedData] = useState([]);
 
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=100")
-      .then(res => res.json()).then(data => setUsers(data))
-      .then(({ results }) => {
-        setUsers(results)
-      });
+      .then(res => res.json()).then(data => setUsers(data.results));
   }, [])
+  console.log(users);
+
+  // const filterUpdated = ((newData, filterConfiguration) => {
+  //     setUpdatedData(newData)
+  // });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <employeeTable />
+    <React.Fragment>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            Employee Database
+        </h1>
+          <p className="subtitle">
+            Sort Employees <strong>by country</strong>!
+        </p>
+          <table>
+            <tr>
+              {/* <TableFilter
+                rows={users}
+                onFilterUpdate={this._filterUpdated}> */}
+                {/* <th filterkey="name"> */}
+                 <th> Employee Name
+  </th>
+                {/* <th filterkey="location"> */}
+                <th>
+                  Location
+  </th>
+                <th>
+                  Phone Number
+  </th>
+              {/* </TableFilter> */}
+            </tr>
+            <tbody>
+              {users.map(user => {
+                return (
+                  <tr key={user.login.uuid}>
+                    <td>{user.name.first} {user.name.last}</td>
+                    <td>{user.location.country}</td>
+                    <td>{user.phone}</td>
+                  </tr>
 
-        {users.map(user => {
-          return (
-            <div>
-              <h4>{user.name.first} {user.name.last}</h4>
-              Phone: <h5>{user.phone}</h5>
-            </div>
-
-          )
-
-        })}
-
-      </header>
-    </div>
-  );
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </React.Fragment >
+  )
 }
 
 export default App;
